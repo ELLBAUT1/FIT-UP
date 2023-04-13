e24f83fa8b03c1aad8f4179068d9b7ff9ac794c8
ELLBAUT1 <117045340+ELLBAUT1@users.noreply.github.com>
2023-04-13 20:48:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -46,23 +46,3 @@ function isValidEmail(email) {
   return re.test(email);
 }
 
-const navLinks = document.querySelectorAll('nav a');
-const sections = document.querySelectorAll('section');
-
-window.addEventListener('scroll', () => {
-  let current = '';
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-    if (pageYOffset >= sectionTop - sectionHeight / 3) {
-      current = section.getAttribute('id');
-    }
-  });
-
-  navLinks.forEach((link) => {
-    link.classList.remove('active');
-    if (link.getAttribute('href') === `#${current}`) {
-      link.classList.add('active');
-    }
-  });
-});
